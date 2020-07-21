995b90153d29074a86c46aea8f56f14bbbd9545d
NourEldin Yasser <a-noyass@microsoft.com>
2020-07-21 22:42:33 +0200

    Add xhtml parser

--- src/main/java/com/example/demo/controller/Parser.java
+++ src/main/java/com/example/demo/controller/Parser.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.xml.sax.SAXException;
@@ -15,6 +16,7 @@ import com.example.demo.service.ParsingService;
 import com.example.demo.service.StorageService;
 
 @RestController
+@RequestMapping("parse")
 public class Parser {
 
     @Autowired
@@ -28,11 +30,19 @@ public class Parser {
         return "test";
     }
 
-    @GetMapping("/parse")
-    public ResponseEntity<?> parseDocument(@RequestParam String fileName) throws IOException, SAXException, TikaException {
+    @GetMapping("/text")
+    public ResponseEntity<?> parseDocumentToPlainText(@RequestParam String fileName) throws IOException, SAXException, TikaException {
         byte[] file = storageService.getFileAsByteArray(fileName);
         String text = parsingService.parseToPlainText(file);
         storageService.writeFile(fileName + ".txt", text);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/xhtml")
+    public ResponseEntity<?> parseDocumentToXHTML(@RequestParam String fileName) throws IOException, SAXException, TikaException {
+        byte[] file = storageService.getFileAsByteArray(fileName);
+        String text = parsingService.parseToXHTML(file);
+        storageService.writeFile(fileName + ".xhtml", text);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- src/main/java/com/example/demo/service/ParsingService.java
+++ src/main/java/com/example/demo/service/ParsingService.java
@@ -8,22 +8,39 @@ import org.apache.tika.exception.TikaException;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.parser.AutoDetectParser;
 import org.apache.tika.sax.BodyContentHandler;
+import org.apache.tika.sax.ToXMLContentHandler;
 import org.springframework.stereotype.Component;
 import org.xml.sax.SAXException;
+import org.xml.sax.ContentHandler;
 
 @Component
 public class ParsingService {
-    
+
     public String parseToPlainText(byte[] file) throws IOException, SAXException, TikaException {
-        long start =  System.currentTimeMillis();
-        
+        long start = System.currentTimeMillis();
+
         BodyContentHandler handler = new BodyContentHandler(1000000);
-     
+
         AutoDetectParser parser = new AutoDetectParser();
         Metadata metadata = new Metadata();
         try (InputStream stream = new ByteArrayInputStream(file)) {
             parser.parse(stream, handler, metadata);
-            long end =  System.currentTimeMillis();
+            long end = System.currentTimeMillis();
+            System.out.println("time taken: " + (end - start));
+            return handler.toString();
+        }
+    }
+
+    public String parseToXHTML(byte[] file) throws IOException, SAXException, TikaException {
+        long start = System.currentTimeMillis();
+
+        ContentHandler handler = new ToXMLContentHandler();
+
+        AutoDetectParser parser = new AutoDetectParser();
+        Metadata metadata = new Metadata();
+        try (InputStream stream = new ByteArrayInputStream(file)) {
+            parser.parse(stream, handler, metadata);
+            long end = System.currentTimeMillis();
             System.out.println("time taken: " + (end - start));
             return handler.toString();
         }
