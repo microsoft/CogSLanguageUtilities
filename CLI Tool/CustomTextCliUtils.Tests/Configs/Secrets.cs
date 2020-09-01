db4835969657fddbdbb2a51287011c73f260bc78
NourEldin Yasser <noursalem95@gmail.com>
2020-09-01 12:50:14 +0200

    [cli-tool][tests] Renamed env variables

--- CLI Tool/CustomTextCliUtils.Tests/Configs/Secrets.cs
+++ CLI Tool/CustomTextCliUtils.Tests/Configs/Secrets.cs
@@ -4,11 +4,11 @@ namespace Microsoft.CustomTextCliUtils.Tests.Configs
 {
     public class Secrets
     {
-        public static readonly string MSReadCognitiveServiceEndPoint = Environment.GetEnvironmentVariable("MSReadCognitiveServiceEndPoint");
-        public static readonly string MSReadCongnitiveServiceKey = Environment.GetEnvironmentVariable("MSReadCongnitiveServiceKey");
-        public static readonly string StorageAccountConnectionString = Environment.GetEnvironmentVariable("StorageAccountConnectionString");
-        public static readonly string CustomTextKey = Environment.GetEnvironmentVariable("CustomTextKey");
-        public static readonly string CustomTextEndpoint = Environment.GetEnvironmentVariable("CustomTextEndpoint");
-        public static readonly string CustomTextAppId = Environment.GetEnvironmentVariable("CustomTextAppId");
+        public static readonly string MSReadCognitiveServicesEndPoint = Environment.GetEnvironmentVariable("MSREAD_COGNITIVE_SERVICES_ENDPOINT");
+        public static readonly string MSReadCongnitiveServicesKey = Environment.GetEnvironmentVariable("MSREAD_COGNITIVE_SERVICES_KEY");
+        public static readonly string StorageAccountConnectionString = Environment.GetEnvironmentVariable("STORAGE_ACCOUNT_CONNECTION_STRING");
+        public static readonly string CustomTextKey = Environment.GetEnvironmentVariable("CUSTOM_TEXT_KEY");
+        public static readonly string CustomTextEndpoint = Environment.GetEnvironmentVariable("CUSTOM_TEXT_ENDPOINT");
+        public static readonly string CustomTextAppId = Environment.GetEnvironmentVariable("CUSTOM_TEXT_APP_ID");
     }
 }
