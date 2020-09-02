83a69998174446de21b4b6289336b0568c087f1f
NourEldin Yasser <noursalem95@gmail.com>
2020-09-02 17:03:43 +0200

    [cli-tool] add command to load configs from file

---
+++ CLI Tool/CustomTextCliUtils/CommandsLayer/ConfigCommand/ConfigLoadCommand.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using McMaster.Extensions.CommandLineUtils;
+using Microsoft.CustomTextCliUtils.ApplicationLayer.Controllers;
+using Microsoft.CustomTextCliUtils.Configs;
+
+namespace Microsoft.CustomTextCliUtils.CommandsLayer.ConfigCommand
+{
+    [Command("load", Description = "loads app configs from file")]
+    public class ConfigLoadCommand
+    {
+        [Option("--path <absolute_path>", Description = "absolute path to configs file")]
+        public string configsFilePath { get; }
+
+        private int OnExecute(CommandLineApplication app)
+        {
+            // build dependencies
+            var container = DependencyInjectionController.BuildConfigCommandDependencies();
+
+            // run program
+            using (var scope = container.BeginLifetimeScope())
+            {
+                var controller = scope.Resolve<ConfigServiceController>();
+                controller.LoadConfigsFromFile(configsFilePath);
+            }
+
+            return 1;
+        }
+    }
+}
