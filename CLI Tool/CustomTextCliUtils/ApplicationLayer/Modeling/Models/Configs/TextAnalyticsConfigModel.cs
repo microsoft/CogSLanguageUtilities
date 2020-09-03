29806c40feadfb31112ff29a34057dd061062d23
Mohamed Shaban <a-moshab@microsoft.com>
2020-09-03 18:02:37 +0200

    [cli-tool][text analytics] added support for text analytics configs

---
+++ CLI Tool/CustomTextCliUtils/ApplicationLayer/Modeling/Models/Configs/TextAnalyticsConfigModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.CustomTextCliUtils.ApplicationLayer.Factories.Storage;
+using Newtonsoft.Json;
+
+namespace Microsoft.CustomTextCliUtils.ApplicationLayer.Modeling.Models.Configs
+{
+    public class TextAnalyticsConfigModel
+    {
+        [JsonProperty("azure-resource-endpoint")]
+        public string AzureResourceEndpoint { get; set; }
+
+        [JsonProperty("azure-resource-key")]
+        public string AzureResourceKey { get; set; }
+
+        [JsonProperty("default_operations")]
+        public TextAnalyticsDefaultOperationsConfigModel DefaultOperations { get; set; }
+
+        public TextAnalyticsConfigModel()
+        {
+            DefaultOperations = new TextAnalyticsDefaultOperationsConfigModel();
+        }
+    }
+}
---
+++ CLI Tool/CustomTextCliUtils/CommandsLayer/ConfigCommand/Set/ConfigSetTextAnalyticsCommand.cs
@@ -0,0 +1,39 @@
+﻿using Autofac;
+using Microsoft.CustomTextCliUtils.Configs;
+using Microsoft.CustomTextCliUtils.ApplicationLayer.Controllers;
+using McMaster.Extensions.CommandLineUtils;
+using Microsoft.CustomTextCliUtils.Configs.Consts;
+using System.ComponentModel.DataAnnotations;
+
+namespace Microsoft.CustomTextCliUtils.CommandsLayer.ConfigCommand.Set
+{
+    [Command("textanalytics", Description = "sets configs for textanalytics")]
+    public class ConfigSetTextAnalyticsCommand
+    {
+        [Option(CommandOptionTemplate.TextAnalyticsAzureResourceKey, Description = "text analytics azure resource key")]
+        public string? AzureResourceKey { get; }
+        [Option(CommandOptionTemplate.TextAnalyticsAzureResourceEndpoint, Description = "text analytics azure resource endpoint")]
+        public string? AzureResourceEndpoint { get; }
+        [Option(CommandOptionTemplate.TextAnalyticsEnableSentiment, optionType: CommandOptionType.SingleValue, Description = "text analytics enable sentiment by default")]
+        public bool? EnableSentimentByDefault { get; }
+        [Option(CommandOptionTemplate.TextAnalyticsEnableNer, optionType: CommandOptionType.SingleValue, Description = "text analytics enable NER by default")]
+        public bool? EnableNerByDefault { get; }
+        [Option(CommandOptionTemplate.TextAnalyticsEnableKeyphrase, optionType: CommandOptionType.SingleValue, Description = "text analytics enable keyphrase by default")]
+        public bool? EnableKeyphraseByDefault { get; }
+
+        private int OnExecute(CommandLineApplication app)
+        {
+            // build dependencies
+            var container = DependencyInjectionController.BuildConfigCommandDependencies();
+
+            // run program
+            using (var scope = container.BeginLifetimeScope())
+            {
+                var controller = scope.Resolve<ConfigServiceController>();
+                controller.SetTextAnalyticsConfigsAsync(AzureResourceKey, AzureResourceEndpoint, EnableSentimentByDefault, EnableNerByDefault, EnableKeyphraseByDefault).ConfigureAwait(false).GetAwaiter().GetResult();
+            }
+
+            return 1;
+        }
+    }
+}
