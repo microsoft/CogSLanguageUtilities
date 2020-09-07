65adce30c95508b7f227fcf8c7085f7335090685
Mohamed Shaban <a-moshab@microsoft.com>
2020-09-07 14:18:59 +0200

    [cli tool][text analytics] modified response object

--- CLI Tool/CustomTextCliUtils/ApplicationLayer/Services/Concatenation/IConcatenationService.cs
+++ CLI Tool/CustomTextCliUtils/ApplicationLayer/Services/Concatenation/IConcatenationService.cs
@@ -1,13 +1,12 @@
 ﻿using Azure.AI.TextAnalytics;
+using Microsoft.CustomTextCliUtils.ApplicationLayer.Modeling.Models.Chunker;
 using Microsoft.CustomTextCliUtils.ApplicationLayer.Modeling.Models.TextAnalytics;
-using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Microsoft.CustomTextCliUtils.ApplicationLayer.Services.Concatenation
 {
     public interface IConcatenationService
     {
-        public List<TextAnalyticsPredictionChunkInfo> ConcatTextAnalytics(AnalyzeSentimentResultCollection sentimentResponse, RecognizeEntitiesResultCollection nerResponse, ExtractKeyPhrasesResultCollection keyphraseResponse);
+        public List<TextAnalyticsPredictionChunkInfo> ConcatTextAnalytics(ChunkInfo[] chunkedText, AnalyzeSentimentResultCollection sentimentResponse, RecognizeEntitiesResultCollection nerResponse, ExtractKeyPhrasesResultCollection keyphraseResponse);
     }
 }
