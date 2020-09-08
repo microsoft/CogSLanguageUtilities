53b302a776cec2c37d85f3f30c3c0823901d25b9
NourEldin Yasser <noursalem95@gmail.com>
2020-09-08 11:34:18 +0200

    [cli-tool] update readme to include textanalytics

--- CLI Tool/CustomTextCliUtils/Configs/Consts/CommandOptionTemplate.cs
+++ CLI Tool/CustomTextCliUtils/Configs/Consts/CommandOptionTemplate.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Microsoft.CustomTextCliUtils.Configs.Consts
+﻿namespace Microsoft.CustomTextCliUtils.Configs.Consts
 {
     public class CommandOptionTemplate
     {
