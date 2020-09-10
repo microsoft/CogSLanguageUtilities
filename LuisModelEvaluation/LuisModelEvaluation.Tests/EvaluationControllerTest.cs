4aa833f05463d806c8c34bbe6181395eae656137
NourEldin Yasser <noursalem95@gmail.com>
2020-09-10 11:55:20 +0200

    [batch testing] add input validation and test

--- LuisModelEvaluation/LuisModelEvaluation.Tests/EvaluationControllerTest.cs
+++ LuisModelEvaluation/LuisModelEvaluation.Tests/EvaluationControllerTest.cs
@@ -1,19 +1,16 @@
 using Microsoft.LuisModelEvaluation.Controllers;
+using Microsoft.LuisModelEvaluation.Exceptions;
 using Microsoft.LuisModelEvaluation.Models.Input;
-using Newtonsoft.Json;
-using System;
 using System.Collections.Generic;
 using Xunit;
 
 namespace Microsoft.LuisModelEvaluation.Tests
 {
     public class EvaluationControllerTest
-
     {
         [Fact]
-        public void EvaluateClassification()
+        public void EvaluateModelTest()
         {
-
             var entity = new Entity
             {
                 Name = "testEntity",
@@ -22,25 +19,72 @@ namespace Microsoft.LuisModelEvaluation.Tests
                 Children = null
             };
             var entities = new List<Entity>()
-                    {
-                        entity
-                    };
+            {
+                entity
+            };
             var predictionData = new PredictionObject
             {
                 Classification = "testClass",
                 Entities = entities
             };
-            var list = new List<TestingExample>();
-            list.Add(new TestingExample
+            var list = new List<TestingExample>
             {
-                ActualData = predictionData,
-                LabeledData = predictionData
-            });
+                new TestingExample
+                {
+                    ActualData = predictionData,
+                    LabeledData = predictionData
+                }
+            };
             var evaluationController = new EvaluationController();
-            var result = evaluationController.RunOperationInternalAsync(list);
-            var resString = JsonConvert.SerializeObject(result);
-            Console.WriteLine(resString);
+            var result = evaluationController.EvaluateModel(list);
+            Assert.Equal(1, result.EntityModelsStats.Count);
+            Assert.Equal(1, result.IntentModelsStats.Count);
+            foreach (var entityStats in result.EntityModelsStats)
+            {
+                Assert.Equal(1.0, entityStats.Precision);
+                Assert.Equal(1.0, entityStats.Recall);
+                Assert.Equal(1.0, entityStats.FScore);
+            }
+            foreach (var intentStats in result.IntentModelsStats)
+            {
+                Assert.Equal(1.0, intentStats.Precision);
+                Assert.Equal(1.0, intentStats.Recall);
+                Assert.Equal(1.0, intentStats.FScore);
+            }
             return;
+        }
+
+        public static TheoryData InvalidInputTestData()
+        {
+            return new TheoryData<IEnumerable<TestingExample>>
+            {
+                {
+                    null
+                },
+                {
+                    new List<TestingExample>
+                    {
+                        null
+                    }
+                },
+                {
+                    new List<TestingExample>
+                    {
+                        new TestingExample
+                        {
+                            ActualData = null
+                        }
+                    }
+                }
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(InvalidInputTestData))]
+        public void InvalidInputTest(IEnumerable<TestingExample> testData)
+        {
+            var evaluation = new EvaluationController();
+            Assert.Throws<InvalidInputException>(() => evaluation.EvaluateModel(testData));
         }
     }
 }
--- LuisModelEvaluation/LuisModelEvaluation/Controllers/EvaluationController.cs
+++ LuisModelEvaluation/LuisModelEvaluation/Controllers/EvaluationController.cs
@@ -1,32 +1,29 @@
-﻿using Microsoft.LuisModelEvaluation.Models.Input;
+﻿using Microsoft.LuisModelEvaluation.Exceptions;
+using Microsoft.LuisModelEvaluation.Models.Input;
 using Microsoft.LuisModelEvaluation.Models.Result;
 using Microsoft.LuisModelEvaluation.Services;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Microsoft.LuisModelEvaluation.Controllers
 {
     public class EvaluationController
-
     {
-
-        public EvaluationController()
-        {
-        }
-
-        public BatchTestResponse RunOperationInternalAsync(
+        public BatchTestResponse EvaluateModel(
             IEnumerable<TestingExample> testData,
             bool verbose = false,
             IEnumerable<Model> entities = null,
             IEnumerable<Model> classes = null)
         {
+            ValidateInput(testData);
 
-            // Intialize the batch test operation helper
-            var batchTestOperationHelper = new EvaluationService(entities, classes);
+            // Intialize the evaluation service
+            var evaluationService = new EvaluationService(entities, classes);
 
             foreach (var testCase in testData)
             {
                 // Intent model stats aggregation
-                batchTestOperationHelper.AggregateIntentStats(testCase.LabeledData.Classification, testCase.ActualData.Classification);
+                evaluationService.AggregateIntentStats(testCase.LabeledData.Classification, testCase.ActualData.Classification);
 
                 // Prepare utterance stats
                 var utteranceStats = new UtteranceStats
@@ -37,12 +34,12 @@ namespace Microsoft.LuisModelEvaluation.Controllers
                 };
 
                 // Populate False entities and Aggregate Entity MUC model stats
-                batchTestOperationHelper.PopulateUtteranceAndEntityStats(testCase.LabeledData.Entities, testCase.ActualData.Entities, utteranceStats);
+                evaluationService.PopulateUtteranceAndEntityStats(testCase.LabeledData.Entities, testCase.ActualData.Entities, utteranceStats);
             }
 
             // Calculate precision, recall and fScore for Intent models
-            var intentModelsStats = new List<ModelStats>(batchTestOperationHelper.IntentsStats.Count);
-            foreach (var intentConfusion in batchTestOperationHelper.IntentsStats.Values)
+            var intentModelsStats = new List<ModelStats>(evaluationService.IntentsStats.Count);
+            foreach (var intentConfusion in evaluationService.IntentsStats.Values)
             {
                 intentModelsStats.Add(new ModelStats
                 {
@@ -57,8 +54,8 @@ namespace Microsoft.LuisModelEvaluation.Controllers
             }
 
             // Calculate precision, recall and fScore for Entity models
-            var entityModelsStats = new List<ModelStats>(batchTestOperationHelper.EntityStats.Count);
-            foreach (var entitiesEvaluation in batchTestOperationHelper.EntityStats.Values)
+            var entityModelsStats = new List<ModelStats>(evaluationService.EntityStats.Count);
+            foreach (var entitiesEvaluation in evaluationService.EntityStats.Values)
             {
                 entityModelsStats.Add(new ModelStats
                 {
@@ -76,8 +73,23 @@ namespace Microsoft.LuisModelEvaluation.Controllers
             {
                 IntentModelsStats = intentModelsStats,
                 EntityModelsStats = entityModelsStats,
-                UtterancesStats = batchTestOperationHelper.UtterancesStats
+                UtterancesStats = evaluationService.UtterancesStats
             };
+        }
+
+        private void ValidateInput(IEnumerable<TestingExample> testData)
+        {
+            if (testData == null || testData.Count() == 0)
+            {
+                throw new InvalidInputException("testData");
+            }
+            foreach (var example in testData)
+            {
+                if (example?.ActualData == null || example?.LabeledData == null)
+                {
+                    throw new InvalidInputException("ActualData and LabeledData");
+                }
+            }
         }
     }
 }
