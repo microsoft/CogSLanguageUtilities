acffae457e59ddc7877cd011e4ce9800aa774908
NourEldin Yasser <noursalem95@gmail.com>
2020-09-21 16:02:52 +0200

    [cli tool][batch testing] Add mapping for nuget output

--- CLI Tool/CogSLanguageUtilities.Core/Services/Evaluation/BatchTestingService.cs
+++ CLI Tool/CogSLanguageUtilities.Core/Services/Evaluation/BatchTestingService.cs
@@ -1,17 +1,18 @@
-﻿using Microsoft.CogSLanguageUtilities.Definitions.APIs.Services;
+﻿using Microsoft.CogSLanguageUtilities.Core.Helpers.Mappers.EvaluationNuget;
+using Microsoft.CogSLanguageUtilities.Definitions.APIs.Services;
+using Microsoft.CogSLanguageUtilities.Definitions.Models.Evaluation;
 using Microsoft.LuisModelEvaluation.Controllers;
 using Microsoft.LuisModelEvaluation.Models.Input;
-using Microsoft.LuisModelEvaluation.Models.Result;
 using System.Collections.Generic;
 
 namespace Microsoft.CogSLanguageUtilities.Core.Services.Evaluation
 {
     public class BatchTestingService : IBatchTestingService
     {
-        public BatchTestResponse RunBatchTest(IEnumerable<TestingExample> testData)
+        public BatchTestingOutput RunBatchTest(IEnumerable<TestingExample> testData)
         {
             var evaluation = new EvaluationController();
-            return evaluation.EvaluateModel(testData, true);
+            return BatchTestingOutputMapper.MapEvaluationOutput(evaluation.EvaluateModel(testData, true));
         }
     }
 }
--- CLI Tool/CogSLanguageUtilities.Definitions/APIs/Services/IBatchTestingService.cs
+++ CLI Tool/CogSLanguageUtilities.Definitions/APIs/Services/IBatchTestingService.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.LuisModelEvaluation.Models.Input;
-using Microsoft.LuisModelEvaluation.Models.Result;
+﻿using Microsoft.CogSLanguageUtilities.Definitions.Models.Evaluation;
+using Microsoft.LuisModelEvaluation.Models.Input;
 using System.Collections.Generic;
 
 namespace Microsoft.CogSLanguageUtilities.Definitions.APIs.Services
 {
     public interface IBatchTestingService
     {
-        public BatchTestResponse RunBatchTest(IEnumerable<TestingExample> testData);
+        public BatchTestingOutput RunBatchTest(IEnumerable<TestingExample> testData);
     }
 }
